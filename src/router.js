5260351ef2b7f3eec628b0f851504414a079a0d9
Ovidiu Cherecheș <skidding@gmail.com>
2019-02-13 20:09:45 +0200

    Create Router helpers

---
+++ src/router.js
@@ -0,0 +1,15 @@
+import { createBrowserHistory } from "history";
+
+export const history = createBrowserHistory();
+
+export function getIndexFromRouteParams({ index }) {
+  return index !== undefined ? Number(index) : 0;
+}
+
+export function selectQuestion(questionIndex) {
+  history.push(`/${questionIndex}`);
+}
+
+export function showSubmittedPage() {
+  history.push(`/done`);
+}
